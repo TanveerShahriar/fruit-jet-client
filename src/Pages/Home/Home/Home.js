a86ab79e6813f62e72b31c02666b86c7be7b74d6
TanveerShahriar <tanveershahriara@gmail.com>
2022-04-30 03:19:50 +0600

    banner updated

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import Banner from '../Banner/Banner';
 import './Home.css'
 
 const Home = () => {
     return (
-        <Container className='d-flex flex-column-reverse flex-lg-row justify-content-center align-items-center my-4'>
-            <div className='banner-text text-start my-4'>
-                <h2>Grab Our</h2>
-                <h1 className='text-danger fw-bolder'>Fresh Fruits</h1>
-                <p className='text-secondary fs-4'>Best place for buying organic chemical free fruits. Buy fruits at the best deal and enjoy it.</p>
-            </div>
-            <div className='banner-image'>
-                <img className='img-fluid' src={"https://raw.githubusercontent.com/TanveerShahriar/images/main/a11images/banner.jpg"} alt="banner" />
-            </div>
-        </Container>
+        <div>
+            <Banner></Banner>
+        </div>
     );
 };
 
