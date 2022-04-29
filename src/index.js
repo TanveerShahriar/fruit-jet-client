3a788609645a9b2b5950c227a89cf4ca611544f6
TanveerShahriar <tanveershahriara@gmail.com>
2022-04-30 00:34:20 +0600

    banner added

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import { HelmetProvider } from "react-helmet-async";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <HelmetProvider>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </React.StrictMode>
-  </HelmetProvider>
+    </HelmetProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
