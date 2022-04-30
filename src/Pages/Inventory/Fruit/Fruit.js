61c30ff67704bf14564e3261023e90f9042cb18d
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-01 02:30:42 +0600

    inventory delete added

--- src/Pages/Inventory/Fruit/Fruit.js
+++ src/Pages/Inventory/Fruit/Fruit.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Fruit.css'
 
-const Fruit = ({ fruit }) => {
-    const { name, supplier, quantity, price } = fruit;
+const Fruit = ({ fruit, handleDelete }) => {
+    const {_id, name, supplier, quantity, price } = fruit;
     return (
         <tr>
             <td>{name}</td>
             <td>{supplier}</td>
             <td>{quantity}</td>
-            <td>{price}</td>
-            <td><button className='btn btn-outline-danger'>DELETE</button></td>
+            {/* <td>{price}</td> */}
+            <td><button className='btn btn-outline-danger'>...</button></td>
+            <td><button className='btn btn-outline-danger' onClick={() => handleDelete(_id)}>X</button></td>
         </tr>
     );
 };
--- src/Pages/Inventory/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory/Inventory.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import swal from 'sweetalert';
 import Fruit from '../Fruit/Fruit';
 import './Inventory.css'
 
@@ -10,22 +11,45 @@ const Inventory = () => {
         fetch('http://localhost:5000/inventory')
             .then(res => res.json())
             .then(data => setFruits(data));
-    }, [])
+    }, [fruits])
+
+    const handleDelete = id => {
+        swal("Are you sure you want to delete this?", {
+            buttons: ["No!", true],
+        })
+            .then(proceed => {
+                if (proceed) {
+                    const url = `http://localhost:5000/inventory/${id}`;
+                    fetch(url, {
+                        method: 'DELETE'
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            const remaining = fruits.filter(fruit => fruit._id !== id);
+                            setFruits(remaining);
+                        })
+                }
+            })
+    }
+
     return (
         <Container>
             <table className='table text-start my-5'>
                 <thead className='fs-4'>
-                    <th>Name</th>
-                    <th>Supplier</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th></th>
+                    <tr>
+                        <th>Name</th>
+                        <th>Supplier</th>
+                        <th>Qty.</th>
+                        <th>More</th>
+                        <th></th>
+                    </tr>
                 </thead>
                 <tbody>
                     {
                         fruits.map(fruit => <Fruit
                             key={fruit._id}
                             fruit={fruit}
+                            handleDelete={handleDelete}
                         ></Fruit>)
                     }
                 </tbody>
