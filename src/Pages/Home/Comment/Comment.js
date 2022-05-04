ee3bf9dcebf63a2d044fd4c621cb6984d281a39b
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-04 13:40:18 +0600

    comment delete button added

--- src/Pages/Home/Comment/Comment.js
+++ src/Pages/Home/Comment/Comment.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 import './Comment.css'
 
-const Review = ({comment}) => {
-    const {picture, name, description} = comment;
+const Review = ({ comment }) => {
+    const [user] = useAuthState(auth);
+    const { picture, name, description } = comment;
     return (
         <Col className='comment'>
             <Card className='h-100'>
                 <Card.Body>
                     <div className='d-flex'>
-                        <img src={picture} alt="" className='col-6 me-4'/>
+                        <img src={picture} alt="" className='col-6 me-4' />
                         <p className='fs-2 text-color fw-bold'>{name}</p>
                     </div>
                     <p className='text-secondary text-start'>{description}</p>
+                    {
+                        (user?.displayName === name) ?
+                            <div className='w-100 me-auto'>
+                                <button className='btn btn-danger text-white text-decoration-none link'>Delete</button>
+                            </div>
+                            :
+                            <></>
+                    }
                 </Card.Body>
             </Card>
         </Col>
